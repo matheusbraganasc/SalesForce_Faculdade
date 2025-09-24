65d1c8f2024b03f4970d75d8b4661f1ac36ab526
Matheus Nascimento Braga <124395248+matheusbraganasc@users.noreply.github.com>
2025-09-24 13:45:14 -0300

    Ajuste homeForm

--- force-app/main/default/lwc/homeForm/homeForm.js
+++ force-app/main/default/lwc/homeForm/homeForm.js
@@ -1,12 +1,56 @@
 import { LightningElement, track, wire } from 'lwc';
+import getProfessores from '@salesforce/apex/ProfessorController.getProfessores';
+import getCursos from '@salesforce/apex/ProfessorController.getCursos';
+import getAlunos from '@salesforce/apex/AlunoController.getAlunos';
 
 export default class HomeForm extends LightningElement {
-    professorId
+    @track professores = [];
+    @track cursos = [];
+    @track alunos = [];
+    @track selectedProfessorId = '';
+    @track selectedCursoId = '';
 
-    handleProfessorSelecionado(event) {
-        this.professorId = event.detail.recordId;
-        this.dispatchEvent(new CustomEvent('professorselecionaso', {
-            detail: { professorId: this.professorId }
-        }));
+    alunoColumns = [
+        { label: 'Nome', fieldName: 'Name' },
+        { label: 'Status', fieldName: 'Status__c' }
+    ];
+
+    @wire(getProfessores)
+    wiredProfessores({ data, error }) {
+        if (data) {
+            this.professores = data.map(prof => ({
+                label: prof.Name,
+                value: prof.Id
+            }));
+        } else if (error) {
+            console.error('Erro ao buscar professores:', error);
+        }
+    }
+
+    handleProfessorChange(event) {
+        this.selectedProfessorId = event.detail.value;
+        this.selectedCursoId = '';
+        this.alunos = [];
+        getCursos()
+            .then(result => {
+                this.cursos = result.filter(curso => curso.Professor__c === this.selectedProfessorId)
+                    .map(curso => ({ label: curso.Name, value: curso.Id }));
+            })
+            .catch(error => {
+                this.cursos = [];
+                console.error('Erro ao buscar cursos:', error);
+            });
+    }
+
+    handleCursoChange(event) {
+        this.selectedCursoId = event.detail.value;
+        getAlunos({ statusMatricula: 'Todos' })
+            .then(result => {
+                this.alunos = result.filter(aluno => aluno.Curso__c === this.selectedCursoId);
+            })
+            .catch(error => {
+                this.alunos = [];
+                console.error('Erro ao buscar alunos:', error);
+            });
     }
 }
